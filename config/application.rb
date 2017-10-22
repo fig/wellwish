f2bdf3c4bd37e7e711c8dab4a4659fc31c8bc486
Sheldon Johnson <shayolden@hotmail.com>
2017-10-22 22:11:42 +1100

    Fix all rubocop violations

--- config/application.rb
+++ config/application.rb
@@ -9,12 +9,13 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module Wellwish
-  class Application < Rails::Application
+  class Application < Rails::Application # :nodoc:
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Settings in config/environments/* take precedence over those specified
+    # here.  Application configuration should go into files in
+    # config/initializers -- all .rb files in that directory are automatically
+    # loaded.
   end
 end

4c7a26e3fb8be693bc1f295594d2e0fdd6aa4266
Sheldon Johnson <shayolden@hotmail.com>
2017-10-27 17:58:43 +1100

    Add splash page view spec skeleton

---
+++ spec/views/splash/index.html.erb_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe "splash/index" do
+  it "includes WellWish" do
+
+  end
+
+  it "includes a logo" do
+
+  end
+
+  it "includes copy" do
+
+  end
+
+  it "includes an email form field" do
+
+  end
+
+  it "includes a link to the github site" do
+
+  end
+end

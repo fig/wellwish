e3801d1579d31844395186d228034a94f6e612a5
Sheldon Johnson <shayolden@hotmail.com>
2017-10-27 19:46:02 +1100

    Add splash page view spec

--- spec/views/splash/index.html.erb_spec.rb
+++ spec/views/splash/index.html.erb_spec.rb
@@ -3,23 +3,25 @@
 require "rails_helper"
 
 describe "splash/index" do
-  it "includes WellWish" do
-
-  end
-
   it "includes a logo" do
-
+    render
+    expect(rendered).to match /\<img src=.*logo.*\/\>/
   end
 
   it "includes copy" do
-
+    render
+    expect(rendered).to include "WellWish is"
   end
 
   it "includes an email form field" do
-
+    render
+    expect(rendered).to match /<form.*>/
+    expect(rendered).to include "email"
+    expect(rendered).to include "submit"
   end
 
   it "includes a link to the github site" do
-
+    render
+    expect(rendered).to include "https://github.com/WellWish/wellwish"
   end
 end

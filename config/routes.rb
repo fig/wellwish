5998c737d0e4a9fe54e3ccce50c89483a994c672
Sheldon Johnson <shayolden@hotmail.com>
2017-12-29 23:54:32 +0700

    Fix mailling list contact view, and update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :mailing_list_contacts
+  root "mailing_list_contacts#new"
+  resources :mailing_list_contacts, only: [:new, :create, :show]
   devise_for :users
-  # For details on the DSL available within this file, see
-  # http://guides.rubyonrails.org/routing.html
-  root "splash#index"
 end
